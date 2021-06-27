57b9943f79defb09355a2f16cce1489a06f63506
Stanley <aynbunda@gmail.com>
2021-06-27 18:22:28 -0400

    employee questions

--- app.js
+++ app.js
@@ -10,26 +10,56 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const output = []
+const questions = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role in the team?",
+        choices: ["Manager", "Intern", "Engineer"]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter the employee's full name: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID number?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school did the intern attend?",
+        when: function (answers) {
+            return answers.role === "Intern";
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+        when: function (answers) {
+            return answers.role === "Engineer";
+        }
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is the manager's office number?",
+        when: function (answers) {
+            return answers.role === "Manager";
+        }
+    },
+    {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Would you like to add another employee to the team?"
+    },
+]
